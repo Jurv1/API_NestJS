35c8e6c320c698464693a5193e0bb069a82711a1
Jurv1 <bushmanhik931@mail.ru>
2023-05-20 23:20:28 +0300

    edited: main.ts and tsconfig.json

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './exepthions/exeption.filter';
 import { myExceptionFactory } from './exepthions/exeption.factory';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
