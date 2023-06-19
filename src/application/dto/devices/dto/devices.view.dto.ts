f27d4aa7ab7b2d0e6ace5ff75c41cccb8d9a0571
Jurv1 <bushmanhik931@mail.ru>
2023-06-20 01:34:36 +0300

    added new DTO for devices view

---
+++ src/application/dto/devices/dto/devices.view.dto.ts
@@ -0,0 +1,6 @@
+export class DevicesViewDto {
+  ip: string;
+  title: string;
+  lastActiveDate: Date;
+  deviceId: string;
+}
--- src/application/utils/mappers/device.mapper.ts
+++ src/application/utils/mappers/device.mapper.ts
@@ -1,5 +1,7 @@
+import { DevicesViewDto } from '../../dto/devices/dto/devices.view.dto';
+
 export class DeviceMapper {
-  mapDevices(objs: any[]) {
+  mapDevices(objs: any[]): DevicesViewDto[] {
     return objs.map((el) => {
       return {
         ip: el[0].Ip,
