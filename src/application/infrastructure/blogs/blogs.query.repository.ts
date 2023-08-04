2ddcf29649900375f54710e38470ae0cb498ed9c
Jurv1 <bushmanhik931@mail.ru>
2023-08-04 15:09:10 +0300

    updated: changed queries

--- src/application/infrastructure/blogs/blogs.query.repository.ts
+++ src/application/infrastructure/blogs/blogs.query.repository.ts
@@ -1,71 +1,71 @@
 import { Injectable } from '@nestjs/common';
-import { InjectDataSource } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
-import { BlogDocument } from '../../schemas/blogs/schemas/blogs.database.schema';
-import { BlogWithPaginationDto } from '../../dto/blogs/dto/view/blog.with.pagination.dto';
+import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { Blog } from '../../entities/blogs/blog.entity';
 import { User } from '../../entities/users/user.entity';
+import { BannedUsersByBlogger } from '../../entities/users/banned.users.by.blogger.entity';
 
 @Injectable()
 export class BlogsQueryRepository {
-  constructor(@InjectDataSource() private readonly dataSource: DataSource) {}
+  constructor(
+    @InjectDataSource() private readonly dataSource: DataSource,
+    @InjectRepository(Blog) private readonly blogsRepo: Repository<Blog>,
+    @InjectRepository(BannedUsersByBlogger)
+    private readonly bansRepo: Repository<BannedUsersByBlogger>,
+  ) {}
 
   async getAllBlogs(
     filter: { [key: string]: string | boolean },
-    sort: { [key: string]: string },
+    sort: { [key: string]: 'ASC' | 'DESC' },
     pagination: {
       skipValue: number;
       limitValue: number;
       pageSize: number;
       pageNumber: number;
     },
-  ): Promise<Blog[] | null> {
-    return await this.dataSource.query(
-      `
-      SELECT * FROM public."blog" 
-      WHERE "isBanned" = false
-        AND "name" ILIKE $1
-      ORDER BY "${Object.keys(sort)[0]}" ${Object.values(sort)[0]}
-      LIMIT ${pagination.limitValue} OFFSET ${pagination.skipValue};
-      `,
-      [filter['nameTerm']],
-    );
+  ): Promise<[Blog[], number]> {
+    return await this.blogsRepo
+      .createQueryBuilder()
+      .select('blog')
+      .from(Blog, 'blog')
+      .where('isBanned = false AND name ILIKE :name', {
+        name: filter['nameTerm'],
+      })
+      .orderBy(`${Object.keys(sort)[0]}`, `${Object.values(sort)[0]}`)
+      .skip(pagination.skipValue)
+      .take(pagination.limitValue)
+      .getManyAndCount();
   }
 
   async countAllBlogs(filter: {
     [key: string]: string | boolean;
   }): Promise<number> {
-    const result = await this.dataSource.query(
-      `
-      SELECT COUNT(*) FROM public."blog"
-      WHERE "isBanned" = false
-        AND "name" ILIKE $1;
-      `,
-      [filter['nameTerm']],
-    );
-
-    return result[0].count;
+    return await this.blogsRepo
+      .createQueryBuilder()
+      .where('isBanned = false')
+      .andWhere('name ILIKE :name', { name: filter['nameTerm'] })
+      .getCount();
   }
 
   async getAllBlogsForAdmin(
     filter: { [key: string]: string | boolean },
-    sort: { [key: string]: string },
+    sort: { [key: string]: 'ASC' | 'DESC' },
     pagination: {
       skipValue: number;
       limitValue: number;
       pageSize: number;
       pageNumber: number;
     },
-  ): Promise<Blog[] | null> {
-    return await this.dataSource.query(
-      `
-      SELECT * FROM public."blog" AS Blogs
-      WHERE "name" ILIKE $1
-      ORDER BY "${Object.keys(sort)[0]}" ${Object.values(sort)[0]}
-      LIMIT ${pagination.limitValue} OFFSET ${pagination.skipValue};  
-      `,
-      [filter['nameTerm']],
-    );
+  ): Promise<[Blog[], number]> {
+    return await this.blogsRepo
+      .createQueryBuilder()
+      .select('blog')
+      .from(Blog, 'blog')
+      .where('name ILIKE :name', { name: filter['nameTerm'] })
+      .orderBy(`${Object.keys(sort)[0]}`, `${Object.values(sort)[0]}`)
+      .skip(pagination.skipValue)
+      .take(pagination.limitValue)
+      .getManyAndCount();
   }
 
   async countAllBlogsForAdmin(filter: {
@@ -84,7 +84,7 @@ export class BlogsQueryRepository {
 
   async getAllBlogsForBlogger(
     filter: { [key: string]: string | boolean },
-    sort: { [key: string]: string },
+    sort: { [key: string]: 'ASC' | 'DESC' },
     pagination: {
       skipValue: number;
       limitValue: number;
@@ -92,38 +92,34 @@ export class BlogsQueryRepository {
       pageNumber: number;
     },
     userId: string,
-  ): Promise<Blog[] | null> {
-    return await this.dataSource.query(
-      `
-      SELECT * FROM public."blog" AS Blogs
-      WHERE "name" ILIKE $1
-        AND Info."ownerId" = $2
-      ORDER BY "${Object.keys(sort)[0]}" ${Object.values(sort)[0]}
-      LIMIT ${pagination.limitValue} OFFSET ${pagination.skipValue};
-      `,
-      [filter['nameTerm'], userId],
-    );
+  ): Promise<[Blog[], number]> {
+    return await this.blogsRepo
+      .createQueryBuilder()
+      .select()
+      .from(Blog, 'blog')
+      .subQuery()
+      .where('name ILIKE :name', { name: filter['nameTerm'] })
+      .andWhere('ownerId = :id', { id: userId })
+      .orderBy(`${Object.keys(sort)[0]}`, `${Object.values(sort)[0]}`)
+      .skip(pagination.skipValue)
+      .take(pagination.limitValue)
+      .getManyAndCount();
   }
 
   async countBlogsForBlogger(
     filter: { [key: string]: string | boolean },
     userId: string,
-  ): Promise<string> {
-    const result = await this.dataSource.query(
-      `
-      SELECT COUNT(*) FROM public."blog" AS Blogs
-      WHERE Info."ownerId" = $1
-        AND "name" ILIKE $2;
-      `,
-      [userId, filter['nameTerm']],
-    );
-
-    return result[0].count;
+  ): Promise<number> {
+    return await this.blogsRepo
+      .createQueryBuilder()
+      .where('name ILIKE :name', { name: filter['nameTerm'] })
+      .andWhere('ownerId = :id', { id: userId })
+      .getCount();
   }
 
   async getAllBannedUsersForBlogger(
     filter: { [key: string]: string | boolean },
-    sort: { [key: string]: string },
+    sort: { [key: string]: 'ASC' | 'DESC' },
     pagination: {
       skipValue: number;
       limitValue: number;
@@ -131,24 +127,16 @@ export class BlogsQueryRepository {
       pageNumber: number;
     },
     blogId: string,
-  ): Promise<User[] | null> {
-    return this.dataSource.query(
-      `
-      SELECT 
-        Users."id",
-        Users."login",
-        BannedUsers."banReason",
-        BannedUsers."banDate"
-      FROM public."banned_users_by_blogger" AS BannedUsers
-       LEFT JOIN public."user" AS Users 
-        ON BannedUsers."userId" = Users."id"
-      WHERE BannedUsers."blogId" = $1
-        AND Users."login" ILIKE $2
-      ORDER BY "${Object.keys(sort)[0]}" ${Object.values(sort)[0]}
-      LIMIT ${pagination.limitValue} OFFSET ${pagination.skipValue};
-      `,
-      [blogId, filter['loginTerm']],
-    );
+  ): Promise<Blog[]> {
+    return await this.blogsRepo
+      .createQueryBuilder('bans')
+      .leftJoinAndSelect('bans.bannedUsersByBlogger', 'u')
+      .where('bans.blogId = :id', { id: blogId })
+      .andWhere('u.login ILIKE :login', { login: filter['loginTerm'] })
+      .orderBy(`${Object.keys(sort)[0]}`, `${Object.values(sort)[0]}`)
+      .skip(pagination.skipValue)
+      .take(pagination.limitValue)
+      .getMany();
   }
 
   async getAllBannedForBlogWithoutFilters(
@@ -189,34 +177,21 @@ export class BlogsQueryRepository {
     return counts[0].count;
   }
 
-  async getOneBlog(id: string): Promise<Blog[] | null> {
-    return this.dataSource.query(
-      `
-      SELECT * FROM public."blog"
-      WHERE "id" = $1 
-        AND "isBanned" = false;
-      `,
-      [id],
-    );
+  async getOneBlog(id: string): Promise<Blog | null> {
+    return this.blogsRepo.findOne({ where: { id: +id, isBanned: false } });
   }
 
   async getOwnerIdAndBlogIdForBlogger(id: string): Promise<Blog[] | null> {
-    return this.dataSource.query(
-      `
-      SELECT "ownerId", "id" FROM public."blog"
-      WHERE "id" = $1;
-      `,
-      [id],
-    );
+    return this.blogsRepo
+      .createQueryBuilder()
+      .select('ownerId')
+      .addSelect('id')
+      .from(Blog, 'blog')
+      .where('id = :id', { id: id })
+      .getRawOne();
   }
 
-  async getOneBlogForAdmin(id: string): Promise<Blog[] | null> {
-    return this.dataSource.query(
-      `
-      SELECT * FROM public."blog"
-      WHERE "id" = $1;
-      `,
-      [id],
-    );
+  async getOneBlogForAdmin(id: string): Promise<Blog | null> {
+    return this.blogsRepo.findOne({ where: { id: +id } });
   }
 }
